b0ea0fd6b56f53f3e2d0e66134fedda62b91b943
BernardinoHeRo <bernardino.behero@gmail.com>
2024-05-15 12:50:55 -0600

    Plan de estudios agregado

--- src/components/curricularMap/Plan.jsx
+++ src/components/curricularMap/Plan.jsx
@@ -11,8 +11,8 @@ import { Autoplay, FreeMode, Navigation, Pagination } from 'swiper/modules';
 
 const Plan = ({ data }) => {
     return (
-        <div className='w-screen flex flex-col justify-center items-center bg-gray-300 pb-4'>
-            <h2 className='text-lg md:text-2xl mt-6 font-semibold'>{data.title}</h2>
+        <div className='w-full flex flex-col justify-center items-center bg-gray-300 pb-8'>
+            <h2 className='text-lg md:text-2xl mt-6 font-bold'>{data.title}</h2>
             <div className="w-full m-auto grid gap-2">
                 <Swiper
                     modules={[FreeMode, Pagination, Autoplay, Navigation]}
@@ -31,12 +31,12 @@ const Plan = ({ data }) => {
                         if (key === 'title' || key === 'introduction') return null;
                         return (
                             <SwiperSlide key={key}>
-                                <div className='mb-4 bg-slate-300'>
-                                    <ul className='bg-gray-500'>
-                                        <h2 className='text-lg md:text-2xl mt-6 font-semibold'>{value.title}</h2>
+                                <div className='w-full h-full bg-unsij-daintree-700 border-2 flex items-center justify-center cursor-pointer'>
+                                    <ul className='w-full h-[95%] bg-unsij-black-50 border-2 border-unsij-black-950 px-4'>
+                                        <h2 className='text-lg md:text-2xl pt-2 font-semibold'>{value.title}</h2>
                                         {value.content.map((course, index) => (
-                                            <li key={index} className='mt-2 '>
-                                                <p className='flex flex-row items-center text-base md:text-xl'>
+                                            <li key={index} className='mt-1 '>
+                                                <p className='flex flex-row items-center text-sm md:text-lg'>
                                                     <strong className='mr-2'><GoDash /></strong>{course}
                                                 </p>
                                             </li>
