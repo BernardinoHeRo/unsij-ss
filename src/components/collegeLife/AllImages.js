c90653788dbb6610fc27dcd3cf09f415f8ba3505
BernardinoHeRo <bernardino.behero@gmail.com>
2024-05-29 09:05:37 -0600

    carga de imagenes

--- src/components/collegeLife/AllImages.js
+++ src/components/collegeLife/AllImages.js
@@ -11,14 +11,54 @@ import ids10 from "../../assets/images/life/IDS/img10.jpg";
 import ids11 from "../../assets/images/life/IDS/img11.jpg"
 import ids12 from "../../assets/images/life/IDS/img11.jpg"
 
-import lat1 from "../../assets/images/life/LAT/img1.jpeg";
-import latlogo from "../../assets/images/life/LAT/unsij.png";
+import lat1 from "../../assets/images/life/LAT/img1.jpg"
+import lat2 from "../../assets/images/life/LAT/img2.jpg"
+import lat3 from "../../assets/images/life/LAT/img3.jpg"
+import lat4 from "../../assets/images/life/LAT/img4.png"
+import lat5 from "../../assets/images/life/LAT/img5.jpg"
+import lat6 from "../../assets/images/life/LAT/img6.jpg"
+import lat7 from "../../assets/images/life/LAT/img7.jpg"
+import lat8 from "../../assets/images/life/LAT/img8.jpg"
+import lat9 from "../../assets/images/life/LAT/img9.jpg"
+import lat10 from "../../assets/images/life/LAT/img10.jpg"
+import lat11 from "../../assets/images/life/LAT/img11.jpg"
+import lat12 from "../../assets/images/life/LAT/img12.jpg"
+import lat13 from "../../assets/images/life/LAT/img13.jpg"
+import lat14 from "../../assets/images/life/LAT/img14.jpg"
+import lat15 from "../../assets/images/life/LAT/img15.jpg"
+
 import ifo1 from "../../assets/images/life/IF/img1.jpeg";
-import ifologo from "../../assets/images/life/IF/unsij.png";
+
 import lb1 from "../../assets/images/life/LB/img1.jpg";
-import lblogo from "../../assets/images/life/LB/unsij.png";
+import lb2 from "../../assets/images/life/LB/img2.jpg";
+import lb3 from "../../assets/images/life/LB/img3.jpg";
+import lb4 from "../../assets/images/life/LB/img4.jpg";
+import lb5 from "../../assets/images/life/LB/img5.jpg";
+import lb6 from "../../assets/images/life/LB/img6.jpg";
+import lb7 from "../../assets/images/life/LB/img7.jpg";
+import lb8 from "../../assets/images/life/LB/img8.jpg";
+import lb9 from "../../assets/images/life/LB/img9.jpg";
+import lb10 from "../../assets/images/life/LB/img10.jpg";
+import lb11 from "../../assets/images/life/LB/img11.jpg";
+import lb12 from "../../assets/images/life/LB/img12.jpg";
+
+
 import lca1 from "../../assets/images/life/LCA/img1.jpg";
-import lcalogo from "../../assets/images/life/LCA/unsij.png";
+import lca2 from "../../assets/images/life/LCA/img2.jpg";
+import lca3 from "../../assets/images/life/LCA/img3.jpg";
+import lca4 from "../../assets/images/life/LCA/img4.jpg";
+import lca5 from "../../assets/images/life/LCA/img5.jpg";
+import lca6 from "../../assets/images/life/LCA/img6.jpg";
+import lca7 from "../../assets/images/life/LCA/img7.jpg";
+import lca8 from "../../assets/images/life/LCA/img8.jpg";
+import lca9 from "../../assets/images/life/LCA/img9.jpg";
+import lca10 from "../../assets/images/life/LCA/img10.jpg";
+import lca11 from "../../assets/images/life/LCA/img11.jpg";
+import lca12 from "../../assets/images/life/LCA/img12.jpg";
+import lca13 from "../../assets/images/life/LCA/img13.jpg";
+import lca14 from "../../assets/images/life/LCA/img14.jpg";
+import lca15 from "../../assets/images/life/LCA/img15.jpg";
+
 
 const images = {
     ids: [
@@ -90,19 +130,9 @@ const images = {
             description: "Description 1"
         },
         {
-            src: ifologo,
-            title: "Title 2",
-            description: "Description 2"
-        },
-        {
             src: ifo1,
             title: "Title 3",
             description: "Description 3"
-        },
-        {
-            src: ifologo,
-            title: "Title 4",
-            description: "Description 4"
         }
     ],
     lat: [
@@ -112,20 +142,74 @@ const images = {
             description: "Description 1"
         },
         {
-            src: latlogo,
-            title: "Title 2",
-            description: "Description 2"
+            src: lat2,
+            title: "Title 1",
+            description: "Description 1"
         },
         {
-            src: lat1,
-            title: "Title 3",
-            description: "Description 3"
+            src: lat3,
+            title: "Title 1",
+            description: "Description 1"
         },
         {
-            src: latlogo,
-            title: "Title 4",
-            description: "Description 4"
-        }
+            src: lat4,
+            title: "Title 1",
+            description: "Description 1"
+        },
+        {
+            src: lat5,
+            title: "Title 1",
+            description: "Description 1"
+        },
+        {
+            src: lat6,
+            title: "Title 1",
+            description: "Description 1"
+        },
+        {
+            src: lat7,
+            title: "Title 1",
+            description: "Description 1"
+        },
+        {
+            src: lat8,
+            title: "Title 1",
+            description: "Description 1"
+        },
+        {
+            src: lat9,
+            title: "Title 1",
+            description: "Description 1"
+        },{
+            src: lat10,
+            title: "Title 1",
+            description: "Description 1"
+        },
+        {
+            src: lat11,
+            title: "Title 1",
+            description: "Description 1"
+        },
+        {
+            src: lat12,
+            title: "Title 1",
+            description: "Description 1"
+        },
+        {
+            src: lat13,
+            title: "Title 1",
+            description: "Description 1"
+        },
+        {
+            src: lat14,
+            title: "Title 1",
+            description: "Description 1"
+        },
+        {
+            src: lat15,
+            title: "Title 1",
+            description: "Description 1"
+        },
     ],
     lb: [
         {
@@ -134,20 +218,60 @@ const images = {
             description: "Description 1"
         },
         {
-            src: lblogo,
+            src: lb2,
             title: "Title 2",
-            description: "Description 2"
+            description: "Description 1"
         },
         {
-            src: lb1,
+            src: lb3,
             title: "Title 3",
-            description: "Description 3"
+            description: "Description 1"
         },
         {
-            src: lblogo,
+            src: lb4,
             title: "Title 4",
-            description: "Description 4"
-        }
+            description: "Description 1"
+        },
+        {
+            src: lb5,
+            title: "Title 5",
+            description: "Description 1"
+        },
+        {
+            src: lb6,
+            title: "Title 6",
+            description: "Description 1"
+        },
+        {
+            src: lb7,
+            title: "Title 7",
+            description: "Description 1"
+        },
+        {
+            src: lb8,
+            title: "Title 8",
+            description: "Description 1"
+        },
+        {
+            src: lb9,
+            title: "Title 9",
+            description: "Description 1"
+        },
+        {
+            src: lb10,
+            title: "Title 10",
+            description: "Description 1"
+        },
+        {
+            src: lb11,
+            title: "Title 11",
+            description: "Description 1"
+        },
+        {
+            src: lb12,
+            title: "Title 12",
+            description: "Description 1"
+        },
     ],
     lca: [
         {
@@ -156,20 +280,75 @@ const images = {
             description: "Description 1"
         },
         {
-            src: lcalogo,
-            title: "Title 2",
-            description: "Description 2"
-        },
-        {
-            src: lca1,
+            src: lca2,
             title: "Title 3",
             description: "Description 3"
         },
         {
-            src: lcalogo,
-            title: "Title 4",
-            description: "Description 4"
-        }
+            src: lca3,
+            title: "Title 3",
+            description: "Description 3"
+        },
+        {
+            src: lca4,
+            title: "Title 3",
+            description: "Description 3"
+        },
+        {
+            src: lca5,
+            title: "Title 3",
+            description: "Description 3"
+        },
+        {
+            src: lca6,
+            title: "Title 3",
+            description: "Description 3"
+        },
+        {
+            src: lca7,
+            title: "Title 3",
+            description: "Description 3"
+        },
+        {
+            src: lca8,
+            title: "Title 3",
+            description: "Description 3"
+        },
+        {
+            src: lca9,
+            title: "Title 3",
+            description: "Description 3"
+        },
+        {
+            src: lca10,
+            title: "Title 3",
+            description: "Description 3"
+        },
+        {
+            src: lca11,
+            title: "Title 3",
+            description: "Description 3"
+        },
+        {
+            src: lca12,
+            title: "Title 3",
+            description: "Description 3"
+        },
+        {
+            src: lca13,
+            title: "Title 3",
+            description: "Description 3"
+        },
+        {
+            src: lca14,
+            title: "Title 3",
+            description: "Description 3"
+        },
+        {
+            src: lca15,
+            title: "Title 3",
+            description: "Description 3"
+        },
     ]
 };
 
